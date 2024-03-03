de2ecc57be31bf3047dd5adcccb3c730f452e774
Reveno <77919585+Reveno@users.noreply.github.com>
2024-03-03 16:32:53 +0200

    Add files via upload

---
+++ js/napoleonic.js
@@ -0,0 +1,6 @@
+const images = document.querySelectorAll('.zoomable');
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        image.classList.toggle('zoomed');
+    });
+});
